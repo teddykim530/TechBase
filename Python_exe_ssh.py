0c9e4abe960207900bb1fb0310b00eceb6e63099
teddy <teddykim530@gmail.com>
2021-11-12 15:16:26 +0900

    update python code

---
+++ Python_exe_ssh.py
@@ -0,0 +1,22 @@
+import os
+
+os.system('C:\\Users\\teddy\\source\\repos\\Project4\\x64\\Debug\\Project4.exe')
+
+import paramiko
+
+try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    ssh.connect("127.0.0.1", port="22", username="teddy", password="gusdn147")
+    print("connected!!")
+    
+    stdin, stdout, stderr = ssh.exec_command("ls")
+    lines = stdout.readlines()
+    resultData = ''.join(lines)
+    
+    print(resultData)
+                                                                            
+    ssh.close()
+    
+except Exception as err:
+        print(err)
